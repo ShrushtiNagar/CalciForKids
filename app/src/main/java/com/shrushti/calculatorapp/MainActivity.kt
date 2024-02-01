0374d42c8a6ffc2660339b9046bc883297d7d86b
Shrushti Nagar <Shrushtinagar220@gmail.com>
2024-02-01 15:15:54 +0530

    May be Final Commit before publishing the app

--- app/src/main/java/com/shrushti/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/shrushti/calculatorapp/MainActivity.kt
@@ -2,121 +2,291 @@ package com.shrushti.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
 import com.shrushti.calculatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var newOp = true
+    private var addDecimal = true
+    private var newOp = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn5.setOnClickListener { numberEvent(binding.btn5) }
-        binding.btn6.setOnClickListener { numberEvent(binding.btn6) }
-        binding.btn7.setOnClickListener { numberEvent(binding.btn7) }
-        binding.btn9.setOnClickListener { numberEvent(binding.btn9) }
-        binding.btn10.setOnClickListener { numberEvent(binding.btn10) }
-        binding.btn11.setOnClickListener { numberEvent(binding.btn11) }
-        binding.btn13.setOnClickListener { numberEvent(binding.btn13) }
-        binding.btn14.setOnClickListener { numberEvent(binding.btn14) }
-        binding.btn15.setOnClickListener { numberEvent(binding.btn15) }
-        binding.btn17.setOnClickListener { numberEvent(binding.btn17) }
-        binding.btn18.setOnClickListener { numberEvent(binding.btn18) }
-        binding.btn2.setOnClickListener { numberEvent(binding.btn2) }
+        MobileAds.initialize(this) {}
+        val adRequest = AdRequest.Builder().build()
+        val adRequest2 = AdRequest.Builder().build()
+        val adRequest3 = AdRequest.Builder().build()
+        val adRequest4 = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
+        binding.adView2.loadAd(adRequest2)
+        binding.adView3.loadAd(adRequest3)
+        binding.adView4.loadAd(adRequest4)
 
-        binding.btn4.setOnClickListener { opEvent(binding.btn4) }
-        binding.btn8.setOnClickListener { opEvent(binding.btn8) }
-        binding.btn12.setOnClickListener { opEvent(binding.btn12) }
-        binding.btn16.setOnClickListener { opEvent(binding.btn16) }
+        binding.adView.adListener = object : AdListener() {
 
-        binding.btn1.setOnClickListener {
-            binding.etResult.setText("0")
-            oldNumber=""
-            newOp = true
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                // Code to be executed when an ad request fails.
+                super.onAdFailedToLoad(adError)
+                binding.adView.loadAd(adRequest)
+            }
+        }
+        binding.adView2.adListener = object : AdListener() {
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                // Code to be executed when an ad request fails.
+                super.onAdFailedToLoad(adError)
+                binding.adView2.loadAd(adRequest2)
+            }
+        }
+        binding.adView3.adListener = object : AdListener() {
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                // Code to be executed when an ad request fails.
+                super.onAdFailedToLoad(adError)
+                binding.adView3.loadAd(adRequest3)
+            }
+        }
+        binding.adView4.adListener = object : AdListener() {
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                // Code to be executed when an ad request fails.
+                super.onAdFailedToLoad(adError)
+                binding.adView4.loadAd(adRequest4)
+            }
         }
 
+        binding.btn5.setOnClickListener {
+            addDigitToEditText("7")
+        }
+        binding.btn6.setOnClickListener {
+            addDigitToEditText("8")
+        }
+        binding.btn7.setOnClickListener {
+            addDigitToEditText("9")
+        }
+        binding.btn9.setOnClickListener {
+            addDigitToEditText("4")
+        }
+        binding.btn10.setOnClickListener {
+            addDigitToEditText("5")
+        }
+        binding.btn11.setOnClickListener {
+            addDigitToEditText("6")
+        }
+        binding.btn13.setOnClickListener {
+            addDigitToEditText("1")
+        }
+        binding.btn14.setOnClickListener {
+            addDigitToEditText("2")
+        }
+        binding.btn15.setOnClickListener {
+            addDigitToEditText("3")
+        }
+        binding.btn17.setOnClickListener {
+            addDigitToEditText("0")
+        }
+        binding.btn1.setOnClickListener {
+            binding.etResult.setText("0")
+            binding.tvResult.text = ""
+        }
+        binding.btn16.setOnClickListener {
+            val value = binding.etResult.text.toString()
+            if (newOp && value != "0" && value.isNotEmpty()) {
+                binding.etResult.append("+")
+                newOp = false
+                addDecimal = true
+            }
+        }
+        binding.btn12.setOnClickListener {
+            val value = binding.etResult.text.toString()
+            if (newOp && value != "0" && value.isNotEmpty()) {
+                binding.etResult.append("-")
+                newOp = false
+                addDecimal = true
+            }
+        }
+        binding.btn8.setOnClickListener {
+            val value = binding.etResult.text.toString()
+            if (newOp && value != "0" && value.isNotEmpty()) {
+                binding.etResult.append("*")
+                newOp = false
+                addDecimal = true
+            }
+        }
         binding.btn3.setOnClickListener {
-            val num = binding.etResult.text.toString().toDouble() / 100
-            binding.etResult.setText(num.toString())
-            newOp = true
+            val value = binding.etResult.text.toString()
+            if (newOp && value != "0" && value.isNotEmpty()) {
+                binding.etResult.append("/")
+                newOp = false
+                addDecimal = true
+            }
+        }
+        binding.btn2.setOnClickListener {
+            val value = binding.etResult.text.toString()
+            if (newOp && value != "0" && value.isNotEmpty()) {
+                binding.etResult.append("%")
+                newOp = false
+                addDecimal = true
+            }
+        }
+        binding.btn4.setOnClickListener {
+            val value = binding.etResult.text.toString()
+            if (value != "0" && value.isNotEmpty()) {
+                val char = value[value.length - 1]
+                if (char == '.') {
+                    addDecimal = true
+                } else if (char == '-' || char == '*' || char == '/' || char == '+') {
+                    newOp = true
+                }
+                binding.etResult.setText(value.subSequence(0, value.length - 1))
+            }
+        }
+        binding.btn18.setOnClickListener {
+            if (addDecimal) {
+                binding.etResult.append(".")
+                addDecimal = false
+            }
         }
 
         binding.btn19.setOnClickListener {
-            var newNumber = binding.etResult.text.toString()
-            var finalNum: Double? = null
-            try {
-                when (op) {
-                    "*" -> {
-                        finalNum = oldNumber.toDouble() * newNumber.toDouble()
-                    }
+            val value = binding.etResult.text.toString()
+            if (value != "0" && value.isNotEmpty() && newOp) {
+                val res = calculations(value)
+                binding.tvResult.text = res
+                addDecimal = true
 
-                    "/" -> {
-                        finalNum = oldNumber.toDouble() / newNumber.toDouble()
-                    }
+            } else {
+                Toast.makeText(this, "Invalid Format", Toast.LENGTH_SHORT).show()
+            }
+        }
 
-                    "+" -> {
-                        finalNum = oldNumber.toDouble() + newNumber.toDouble()
-                    }
 
-                    "-" -> {
-                        finalNum = oldNumber.toDouble() - newNumber.toDouble()
-                    }
+    }
+
+
+    private fun calculations(value: String): String {
+        var listOfDigits = divideString(value)
+        if (listOfDigits.isEmpty()) {
+            return ""
+        }
+
+        while (listOfDigits.contains('%')) {
+            listOfDigits = moduloCalc(listOfDigits)
+            if (listOfDigits.isEmpty()) {
+                return ""
+            }
+        }
+        while (listOfDigits.contains('*') || listOfDigits.contains('/')) {
+            listOfDigits = mulDivide(listOfDigits)
+        }
+
+        val result = addSub(listOfDigits)
+        return result.toString()
+    }
+
+    private fun divideString(str: String): MutableList<Any> {
+        val list = mutableListOf<Any>()
+        var currDigit = ""
+        for (i in str) {
+            if (i.isDigit() || i == '.') {
+                currDigit += i
+            } else {
+                list.add(currDigit.toFloat())
+                list.add(i)
+                currDigit = ""
+            }
+        }
+        if (currDigit != "") list.add(currDigit.toFloat())
+        return list
+    }
+
+    private fun moduloCalc(list: MutableList<Any>): MutableList<Any> {
+        val newList = mutableListOf<Any>()
+
+        var restartIndex = list.size
+
+        for (i in list.indices) {
+
+            if (list[i] is Char && i < restartIndex) {
+                val prev = list[i - 1] as Float
+                val next = list[i + 1] as Float
+
+                if (list[i] == '%') {
+                    val res = prev % next
+                    newList.add(res)
+                    restartIndex = i + 1
+                } else {
+                    newList.add(prev)
+                    newList.add(list[i])
                 }
-                binding.etResult.setText(finalNum.toString())
-            } catch (e: Exception) {
-                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
-                binding.etResult.setText("0")
+            } else if (i > restartIndex) {
+                newList.add(list[i])
             }
-
-            newOp = true
-            op = ""
         }
-
+        return newList
     }
 
-    private var minus = true
-    private var dot = true
-    private fun numberEvent(btn: Button?) {
+    private fun mulDivide(list: MutableList<Any>): MutableList<Any> {
+        val newList = mutableListOf<Any>()
+        var restartIndex = list.size
+        for (i in list.indices) {
+            if (list[i] is Char && i < restartIndex) {
+                val prev = list[i - 1] as Float
+                val op = list[i]
+                val next = list[i + 1] as Float
 
-        if (newOp) {
-            binding.etResult.text.clear()
-            newOp = false
-            minus = true
-            dot = true
-        }
-        var text = binding.etResult.text.toString()
-
-        if(btn == binding.btn18){
-            if(dot){
-                text+= btn!!.text
-                dot = false
+                if (op == '*') {
+                    val result = prev * next
+                    newList.add(result)
+                    restartIndex = i + 1
+                } else if (op == '/') {
+                    val result = prev / next
+                    newList.add(result)
+                    restartIndex = i + 1
+                } else {
+                    newList.add(prev)
+                    newList.add(op)
+                }
+            } else if (i > restartIndex) {
+                newList.add(list[i])
             }
-
         }
-        else if (btn!! == binding.btn2) {
-            if(minus){
-                text = "-$text"
-                minus = false
-            }
-
-        } else text += btn!!.text
-
-        binding.etResult.setText(text)
-
+        return newList
     }
 
-    private var oldNumber = ""
-    private var op = ""
+    private fun addSub(list: MutableList<Any>): Float {
+        var result = list[0] as Float
 
-    private fun opEvent(btn: Button?) {
+        for (i in list.indices) {
+            if (list[i] is Char) {
+                val op = list[i]
+                val next = list[i + 1] as Float
+                when (op) {
+                    '+' -> result += next
+                    '-' -> result -= next
+                }
+            }
+        }
+        return result
+    }
+
+    private fun addDigitToEditText(digit: String) {
+        var value = binding.etResult.text.toString()
+        if (value != "0" && value.isNotEmpty()) {
+            value += digit
+        } else {
+            value = digit
+        }
+        binding.etResult.setText(value)
+        addDecimal = true
         newOp = true
-        op = btn!!.text.toString()
-        oldNumber = binding.etResult.text.toString()
-
     }
 
 }
---
+++ app/src/main/java/com/shrushti/calculatorapp/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.shrushti.calculatorapp
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import androidx.core.os.postDelayed
+
+class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(5000){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
